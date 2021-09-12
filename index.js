829a2f3cb62dfa01777539b887456d1f2b45545f
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2021-09-12 12:54:04 +0200

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+addEventListener("fetch", (event) => {
+  globalThis.count = (globalThis.count ?? 0) + 1;
+  const response = new Response(globalThis.count.toString(), {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
