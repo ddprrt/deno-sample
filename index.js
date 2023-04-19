6e2d9a25d821c6ad4ef16a3dbd9dcb23c41c8d04
Stefan Baumgartner <sbaumg@gmail.com>
2023-04-19 11:11:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 addEventListener("fetch", (event) => {
+  while(true){}
   globalThis.count = (globalThis.count ?? 0) + 1;
   const response = new Response(globalThis.count.toString(), {
     headers: { "content-type": "text/plain" },
